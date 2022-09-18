cfb46b884772decb2ecf23066b5946c1afa76d75
AndSchmitz <andreas.schmitz@posteo.de>
2022-09-18 10:23:25 +0200

    Exported two additional functions

--- R/CalculateFrictionVelocity.R
+++ R/CalculateFrictionVelocity.R
@@ -1,35 +1,76 @@
-#Friction velocity
-#Erisman JW, Draaijers GPJ. Atmospheric Deposition In Relation to Acidification and
-#Eutrophication. 1995. Page 67.
+#' @title CalculateFrictionVelocity
+#'
+#' @description Calculates friction velocity according to Erisman and Draaijers (1995) page 67 equation 3.25.
+#'
+#' @param WindSpeed_ms Wind speed in m/s.
+#'
+#' @param ZeroPlaneDisplacementHeight_m Displacement height in m.
+#'
+#' @param RoughnessLength_m Roughness length in m.
+#'
+#' @param MoninObukhovLength_m Monin-Obkukhiv length in m. Monin-Obkukhiv length for neutral stratification (Pasquill class D) is "infinity". This case is encoded by a value defined in GetConstants()$InfLength in this package.
+#'
+#' @return A friction velocity value in m.
+#'
+#' @examples
+#'
+#' # Friction velocity over grassland for extremely unstable stratification
+#' # (Pasquill class A)
+#' MOL_m <- CalculateMoninObukhovLength(
+#'   PasquillClass = "A",
+#'   RoughnessLength_m = 0.03
+#' )
+#' CalculateFrictionVelocity(
+#'   WindSpeed_ms = 5,
+#'   AnemometerHeight_m = 10,
+#'   ZeroPlaneDisplacementHeight_m = 0.21,
+#'   RoughnessLength_m = 0.03,
+#'   MoninObukhovLength_m = MOL_m
+#' )
+#'
+#' # Friction velocity over grassland for neutral stratification
+#' # (Pasquill class D)
+#' MOL_m <- CalculateMoninObukhovLength(
+#'   PasquillClass = "D",
+#'   RoughnessLength_m = 0.03
+#' )
+#' CalculateFrictionVelocity(
+#'   WindSpeed_ms = 5,
+#'   AnemometerHeight_m = 10,
+#'   ZeroPlaneDisplacementHeight_m = 0.21,
+#'   RoughnessLength_m = 0.03,
+#'   MoninObukhovLength_m = MOL_m
+#' )
+#' @export
+#' @references Erisman JW, Draaijers GPJ. Atmospheric Deposition In Relation to Acidification and Eutrophication. 1995.
 
-CalculateFrictionVelocity <- function(
-  WindSpeed_ms,
-  AnemometerHeight_m,
-  ZeroPlaneDisplacementHeight_m,
-  RoughnessLength_m,
-  MoninObukhovLength_m
-) {
 
-  #Propagate NA
+CalculateFrictionVelocity <- function(WindSpeed_ms,
+                                      AnemometerHeight_m,
+                                      ZeroPlaneDisplacementHeight_m,
+                                      RoughnessLength_m,
+                                      MoninObukhovLength_m) {
+
+  # Propagate NA
   if (
     is.na(WindSpeed_ms) |
-    is.na(AnemometerHeight_m) |
-    is.na(ZeroPlaneDisplacementHeight_m) |
-    is.na(RoughnessLength_m) |
-    is.na(MoninObukhovLength_m)
+      is.na(AnemometerHeight_m) |
+      is.na(ZeroPlaneDisplacementHeight_m) |
+      is.na(RoughnessLength_m) |
+      is.na(MoninObukhovLength_m)
   ) {
     return(NA)
   }
 
-  #Catch case AnemometerHeight_m < (ZeroPlaneDisplacementHeight_m + RoughnessLength_m)
-  #This causes calculation of log(negative number) [if AnemometerHeight_m < ZeroPlaneDisplacementHeight_m] or
-  #log(<1) which would result in a negative value of friction velocity.
-  if ( AnemometerHeight_m < (ZeroPlaneDisplacementHeight_m + RoughnessLength_m) ) {
+  # Catch case AnemometerHeight_m < (ZeroPlaneDisplacementHeight_m + RoughnessLength_m)
+  # This causes calculation of log(negative number) [if AnemometerHeight_m < ZeroPlaneDisplacementHeight_m] or
+  # log(<1) which would result in a negative value of friction velocity.
+  if (AnemometerHeight_m < (ZeroPlaneDisplacementHeight_m + RoughnessLength_m)) {
     stop("AnemometerHeight_m must not be lower than (ZeroPlaneDisplacementHeight_m + RoughnessLength_m).")
   }
 
 
-  #Van Karman constant is defined in helping function GetConstants()
+  # Van Karman constant is defined in helping function GetConstants()
   kappa <- GetConstants()$kappa
 
   StabilityCorrectionForMomentum1 <- CalculateStabilityCorrection(
@@ -43,11 +84,11 @@ CalculateFrictionVelocity <- function(
     Type = "Momentum"
   )
 
-  #log() is natural logarithm (ln()) as in publication ED95.
-  FrictionVelocity_ms <- kappa * WindSpeed_ms / ( log((AnemometerHeight_m - ZeroPlaneDisplacementHeight_m)/RoughnessLength_m) - StabilityCorrectionForMomentum1 + StabilityCorrectionForMomentum2 )
-  FrictionVelocity_ms <- round(FrictionVelocity_ms,GetConstants()$RoundingPrecision)
+  # log() is natural logarithm (ln()) as in publication ED95.
+  FrictionVelocity_ms <- kappa * WindSpeed_ms / (log((AnemometerHeight_m - ZeroPlaneDisplacementHeight_m) / RoughnessLength_m) - StabilityCorrectionForMomentum1 + StabilityCorrectionForMomentum2)
+  FrictionVelocity_ms <- round(FrictionVelocity_ms, GetConstants()$RoundingPrecision)
 
-  if ( is.na(FrictionVelocity_ms) ) {
+  if (is.na(FrictionVelocity_ms)) {
     stop("Calculation of friction velocity failed.")
   }
   return(FrictionVelocity_ms)
--- R/CalculateMoninObukhovLength.R
+++ R/CalculateMoninObukhovLength.R
@@ -1,40 +1,65 @@
-#Monin-Obukhov-Length
-#Seinfeld JH, Pandis SN. Atmospheric Chemistry and Physics: From Air Pollution to Climate Change.
-#2006. Ppage 751.
+#' @title CalculateMoninObukhovLength
+#'
+#' @description Calculates Monin-Obukhov length according to Seinfeld and Pandis
+#' (2006) page 751 eq. 16.83.
+#'
+#' @param PasquillClass A single character indicating the Pasquill stability class. For example according to Seinfeld and Pandis
+#' (2006) page 750 as implemented in GetPasquillClass().
+#'
+#' @param RoughnessLength_m Roughness length in m.
+#'
+#' @return A numeric value for Monin-Obukhov length. Monin-Obkukhiv length for neutral stratification (Pasquill class D) is "infinity". This case is encoded by a value defined in GetConstants()$InfLength in this package.
+#'
+#' @examples
+#'
+#' # For Pasquill class A ("extremely unstable" conditions) and grassland
+#' CalculateMoninObukhovLength(
+#'   PasquillClass = "A",
+#'   RoughnessLength_m = 0.03
+#' )
+#'
+#' # For Pasquill class D ("neutral" conditions) and grassland
+#' CalculateMoninObukhovLength(
+#'   PasquillClass = "D",
+#'   RoughnessLength_m = 0.03
+#' )
+#' GetConstants()$InfLength
+#'
+#' @export
+#' @references Seinfeld JH, Pandis SN. Atmospheric Chemistry and Physics: From Air Pollution to Climate Change 2006.
+#'
 
-CalculateMoninObukhovLength <- function(
-  PasquillClass,
-  z_0
-) {
+CalculateMoninObukhovLength <- function(PasquillClass,
+                                        RoughnessLength_m) {
   PasquillObukhovPars <- tribble(
     ~PasquillClass, ~a, ~b,
-    "A",-0.096,0.029,
-    "B",-0.037,0.029,
-    "C",-0.002,0.018,
-    "D",0,0,
-    "E",0.004,-0.018,
-    "F",0.035,-0.036
+    "A", -0.096, 0.029,
+    "B", -0.037, 0.029,
+    "C", -0.002, 0.018,
+    "D", 0, 0,
+    "E", 0.004, -0.018,
+    "F", 0.035, -0.036
   )
-  
-  if ( !(PasquillClass %in% PasquillObukhovPars$PasquillClass) ) {
-    stop(paste("Error in function CalculateMoninObukhovLength(): Pasquill class",PasquillClass,"not implemented."))
+
+  if (!(PasquillClass %in% PasquillObukhovPars$PasquillClass)) {
+    stop(paste("Error in function CalculateMoninObukhovLength(): Pasquill class", PasquillClass, "not implemented."))
   }
-  
+
   idx <- which(PasquillObukhovPars$PasquillClass == PasquillClass)
   a <- PasquillObukhovPars$a[idx]
   b <- PasquillObukhovPars$b[idx]
-  if ( (a==0) & (b==0) ) {
-    #Monin-Obukhov-length goes to infinity in the limit of perfectly neutral
-    #conditions. See for example Erisman JW, Draaijers GPJ. Atmospheric Deposition In Relation
-    #to Acidification and Eutrophication. 1995. Page 66.
-    #This case is coded as GetConstants()$InfLength (-9999)
+  if ((a == 0) & (b == 0)) {
+    # Monin-Obukhov-length goes to infinity in the limit of perfectly neutral
+    # conditions. See for example Erisman JW, Draaijers GPJ. Atmospheric Deposition In Relation
+    # to Acidification and Eutrophication. 1995. Page 66.
+    # This case is coded as GetConstants()$InfLength
     return(GetConstants()$InfLength)
   } else {
-    #SP06 eq. 16.83
-    #log() in SP06 means log10 as reconstructed from example calculation given
-    #immediately below SP06 eq. 16.83 (page 751)
-    L <- 1 / (a + b * log10(z_0))
-    L <- round(L,GetConstants()$RoundingPrecision)
-    return(L) 
+    # SP06 eq. 16.83
+    # log() in SP06 means log10 as reconstructed from example calculation given
+    # immediately below SP06 eq. 16.83 (page 751)
+    L <- 1 / (a + b * log10(RoughnessLength_m))
+    L <- round(L, GetConstants()$RoundingPrecision)
+    return(L)
   }
 }
